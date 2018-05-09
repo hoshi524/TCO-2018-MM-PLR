1e9e6ff57bd51d29fc9e5e5fdf2deb8d585945b5
hoshi524 <hoshi524@gmail.com>
2018-05-09 20:01:51 +0900

    update

--- src/last.cpp
+++ src/last.cpp
@@ -15,7 +15,7 @@ inline unsigned get_random() {
   return y ^= (y ^= (y ^= y << 13) >> 17) << 5;
 }
 
-constexpr float TIME_LIMIT = 9;
+constexpr float TIME_LIMIT = 2;
 constexpr int MAX_S = 1 << 8;
 constexpr int MAX_N = 1 << 12;
 constexpr int MAX_C = 1 << 3;
@@ -27,7 +27,6 @@ uint8_t oldColors[MAX_S][MAX_S];
 uint8_t ans[MAX_N];
 uint8_t color[MAX_N];
 uint8_t colorOrder[MAX_N][MAX_C];
-uint8_t colorCount[MAX_N][MAX_C];
 uint8_t colorBit[MAX_N];
 uint16_t regions[MAX_S][MAX_S];
 uint16_t edge[MAX_N][MAX_E];
@@ -37,19 +36,50 @@ mt19937 engine(get_random());
 namespace state {
 void set(int r, int c) {
   color[r] = c;
-  for (int i = 1; i <= edge[r][0]; ++i) {
-    int s = edge[r][i];
-    colorCount[s][c]++;
-    colorBit[s] &= ~(1 << c);
+  colorBit[r] = 0;
+  static uint16_t queue[MAX_N];
+  int qs = 0;
+  auto remove = [&](int r, int c) {
+    uint8_t& x = colorBit[r];
+    if (x & (1 << c)) {
+      x ^= 1 << c;
+      if (bitset<MAX_C>(x).count() == 2) queue[qs++] = r;
+    }
+  };
+  for (int i = 1; i <= edge[r][0]; ++i) remove(edge[r][i], c);
+  for (int i = 0; i < qs; ++i) {
+    int r0 = queue[i];
+    int b = colorBit[r0];
+    auto removes = [&](vector<int>& v) {
+      std::set<int> set;
+      int f = v[0];
+      for (int r : v) {
+        std::set<int> next;
+        for (int i = 1; i <= edge[r][0]; ++i) {
+          int s = edge[r][i];
+          if (colorBit[s] == 0) continue;
+          if (f != r and set.find(s) == set.end()) continue;
+          next.insert(s);
+        }
+        set = move(next);
+      }
+      for (int r : set) {
+        for (int c = 0; c < MAX_C; ++c) {
+          if (b & (1 << c)) remove(r, c);
+        }
+      }
+    };
+    int t = bitset<MAX_C>(b).count();
+    if (t == 2) {
+      for (int j = 1; j <= edge[r0][0]; ++j) {
+        int r1 = edge[r0][j];
+        if (b == colorBit[r1]) {
+          vector<int> v{r0, r1};
+          removes(v);
+        }
+      }
+    }
   }
-}
-void reset(int r) {
-  int c = color[r];
-  for (int i = 1; i <= edge[r][0]; ++i) {
-    int s = edge[r][i];
-    if (--colorCount[s][c] == 0) colorBit[s] &= ~(1 << c);
-  }
-  color[r] = -1;
 }
 }  // namespace state
 
@@ -81,6 +111,8 @@ class MapRecoloring {
             connect[b][a] = true;
             edge[a][++edge[a][0]] = b;
             edge[b][++edge[b][0]] = a;
+            // assert(edge[a][0] < MAX_E);
+            // assert(edge[b][0] < MAX_E);
           };
           if (i > 0) add(regions[i][j], regions[i - 1][j]);
           if (j > 0) add(regions[i][j], regions[i][j - 1]);
@@ -102,11 +134,10 @@ class MapRecoloring {
     constexpr int COLOR = 100000;
     int value = INT_MAX;
     for (int i = 0; i < X; ++i) nlist[i] = i;
-    for (int z = 0; z < 100 or value == INT_MAX; ++z) {
+    while (rdtsc() - start < TIME_LIMIT or value == INT_MAX) {
       [&]() {
-        int nc = value == INT_MAX ? 7 : 6;
+        int nc = 6 + (value < 7 * COLOR ? 0 : (get_random() & 1));
         memset(colorBit, (1 << nc) - 1, sizeof(colorBit));
-        memset(colorCount, 0, sizeof(colorCount));
         shuffle(nlist, nlist + X, engine);
         for (int i = 0; i < X; ++i) {
           int n, nv = 0xff;
@@ -138,64 +169,6 @@ class MapRecoloring {
           memcpy(ans, color, sizeof(ans));
         }
       }();
-    }
-    {
-      int cv = value;
-      int nc = value / COLOR;
-      {
-        memset(colorBit, (1 << nc) - 1, sizeof(colorBit));
-        memset(colorCount, 0, sizeof(colorCount));
-        for (int i = 0; i < X; ++i) {
-          state::set(i, ans[i]);
-        }
-      }
-      constexpr int LOG_SIZE = 1 << 10;
-      int log_[LOG_SIZE];
-      while (true) {
-        float time = TIME_LIMIT - (rdtsc() - start);
-        if (time < 0) break;
-        for (int i = 0; i < LOG_SIZE; ++i) {
-          log_[i] = -30 * log((i + 0.5) / LOG_SIZE) * time / TIME_LIMIT;
-        }
-        for (int i = 0; i < 0xff; ++i) {
-          static int queue[2][2];
-          int qs = 0;
-          int v = 0;
-          auto add = [&](int r, int c) {
-            v += regionsColor[r][c] - regionsColor[r][color[r]];
-            queue[qs][0] = r;
-            queue[qs][1] = c;
-            ++qs;
-          };
-          if (get_random() & 1) {
-            int r = get_random() % X;
-            int c = get_random() % nc;
-            if (c == color[r] or colorCount[r][c] > 0) continue;
-            add(r, c);
-          } else {
-            int r0 = get_random() % X;
-            int r1 = edge[r0][1 + get_random() % edge[r0][0]];
-            int c0 = color[r0];
-            int c1 = color[r1];
-            if (colorCount[r0][c1] > 1 or colorCount[r1][c0] > 1) continue;
-            add(r0, c1);
-            add(r1, c0);
-          }
-          if (v <= log_[get_random() & (LOG_SIZE - 1)]) {
-            cv += v;
-            for (int i = 0; i < qs; ++i) {
-              int r = queue[i][0];
-              int c = queue[i][1];
-              state::reset(r);
-              state::set(r, c);
-            }
-            if (value > cv) {
-              value = cv;
-              memcpy(ans, color, sizeof(ans));
-            }
-          }
-        }
-      }
     }
     vector<int> ret(X);
     for (int i = 0; i < X; ++i) ret[i] = ans[i];
